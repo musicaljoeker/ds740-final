f6ec6fc3c4d08549536261d87394179c8829a791
Joseph Kerkhof <joseph@clockwork.com>
2020-05-02 13:35:13 -0500

    wrapping up data cleaning

--- bin/start_tensorflow.sh
+++ bin/start_tensorflow.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#	-u $(id -u):$(id -g) \
 docker run -it -d \
 	-p 8888:8888 \
 	--name tensorflow \
-	--mount src=`pwd`,target=/final,type=bind \
+	--mount src=`pwd`,target=/tf/final,type=bind \
 	--gpus all \
-	tensorflow/tensorflow:2.1.0-gpu-py3
+	tensorflow/tensorflow:2.1.0-gpu-py3-jupyter
 
-docker exec -it tensorflow pip install -r final/tensorflow/requirements.txt
+docker exec -it tensorflow pip install -r /tf/final/tensorflow/requirements.txt
