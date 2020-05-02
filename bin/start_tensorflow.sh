8aae8d38e24e192575bd2f6b4ebfe8c32fd500d1
Joseph Kerkhof <joseph@clockwork.com>
2020-05-02 15:11:06 -0500

    requiring tensorflow_docs

--- bin/start_tensorflow.sh
+++ bin/start_tensorflow.sh
@@ -7,4 +7,8 @@ docker run -it -d \
 	--gpus all \
 	tensorflow/tensorflow:2.1.0-gpu-py3-jupyter
 
+# installing git
+docker exec -u root -it tensorflow apt update
+docker exec -u root -it tensorflow apt install git -y
+
 docker exec -it tensorflow pip install -r /tf/final/tensorflow/requirements.txt
