540ea3052a7e324dacb91592dae4c61e9722b4c9
Joseph Kerkhof <joseph@clockwork.com>
2020-05-01 22:36:38 -0500

    loading the data into a dataframe

--- tensorflow/final.py
+++ tensorflow/final.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,4 +10,21 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+# tf version check
 print(tf.__version__)
+
+# getting the data into python
+cols_to_keep = [
+    "DAY_OF_MONTH",
+    "DAY_OF_WEEK",
+    "OP_CARRIER",
+    "ORIGIN",
+    "DEST",
+    "DEP_DEL15",
+    "DEP_TIME_BLK",
+    "CANCELLED",
+    "DIVERTED",
+    "DISTANCE"
+]
+raw_jan19 = pd.read_csv("data/Jan_2019_ontime.csv")
+print(raw_jan19[cols_to_keep].tail())
